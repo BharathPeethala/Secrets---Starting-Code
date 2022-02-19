8bb5e9188ed61b48e9312cbc444dd4ba463ae574
BharathPeethla <bharathpeethala97@gmail.com>
2022-02-19 15:40:00 +0530

    add environment variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = new require("express");
 const ejs = new require("ejs");
 const bodyParser = new require("body-parser");
@@ -25,8 +26,10 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 });
-let secret = "nakdanienfcaoienrkaljlkienak";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+	secret: process.env.SECRET,
+	encryptedFields: ["password"],
+});
 User = mongoose.model("User", userSchema);
 // Requests
 app.route("/").get((req, res) => {
@@ -40,12 +43,10 @@ app
 	.post((req, res) => {
 		const username = req.body.username;
 		const password = req.body.password;
-		console.log("password entered:" + password);
 		User.findOne({ email: username }, (err, foundUser) => {
 			if (err) console.log(err);
 			else {
 				if (foundUser) {
-					console.log("password in database:" + foundUser.email);
 					if (foundUser.password == password) res.render("secrets");
 					else console.log("Password is incorrect");
 				} else {
